d022c69edfe223ce60e6643fe028be8fbb2dc04f
Seyf89 <73757234+Seyf89@users.noreply.github.com>
2022-08-15 16:40:04 +0200

    First commit

---
+++ src/main/java/com/spring/social/model/Role.java
@@ -0,0 +1,29 @@
+package com.spring.social.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role_name")
+    private String nameRole;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+}
